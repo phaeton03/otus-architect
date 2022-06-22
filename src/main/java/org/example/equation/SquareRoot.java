2a441c041e274167563c3f6612b34f96fb77ff4e
nnikolskiy <nnikolskiy@phoenixit.ru>
2022-06-22 17:03:44 +0300

    в метод solve добавлена проверка на а = 0

--- src/main/java/org/example/equation/SquareRoot.java
+++ src/main/java/org/example/equation/SquareRoot.java
@@ -1,13 +1,21 @@
 package org.example.equation;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import static java.lang.Math.*;
+
+@Component
 public class SquareRoot implements Math {
     @Value("${math.precision.eps}")
     private Double eps;
 
     @Override
     public Double[] solve(Double a, Double b, Double c) {
+        if (abs(a) < eps) {
+            throw new IllegalArgumentException();
+
+        }
         return new Double[0];
     }
 }
--- src/test/java/org/example/equation/SquareRootTest.java
+++ src/test/java/org/example/equation/SquareRootTest.java
@@ -1,19 +1,34 @@
 package org.example.equation;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(value = {MockitoExtension.class})
 class SquareRootTest {
-    private final SquareRoot squareRoot = new SquareRoot();
+    SquareRoot squareRoot = new SquareRoot();
 
     private static final Double ZERO_A = 0.0;
 
+    private final Double EPS = 0.0005;
+
+    @BeforeEach
+    void printEps() {
+        ReflectionTestUtils.setField(squareRoot, "eps", EPS);
+    }
+
     @Test
     void shouldThrowIllegalStateExceptionWhenAIsZero() {
-        assertThatThrownBy(() -> squareRoot.solve(ZERO_A, 5.0, 6.0)).isInstanceOf(IllegalStateException.class);
+
+        assertThatThrownBy(() -> squareRoot.solve(ZERO_A, 5.0, 6.0)).isInstanceOf(IllegalArgumentException.class);
     }
 }
